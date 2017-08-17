cb45c3b13afef4d9f6d7de610a7bf8c30d1c8ff8
Journey <timtag1190@gmail.com>
2017-08-16 21:31:54 -0500

    AxCrypt Package

---
+++ automatic/axcrypt/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'axcrypt'
+$url32 = 'https://www.axcrypt.net/downloads/4519/'
+$checksum32 = '6d2ecebb869be36e0bf2b48f63ea6e7c1e8cf27c8e08f9a892dedb69295c74ba'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = $url32
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/axcrypt/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://www.axcrypt.net/download/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "([$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "([$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $regex = 'https://www.axcrypt.net/downloads/'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 2 -expand href
+
+  $versionRegEx = 'Version\s+(\d+)\.(\d+)\.(\d+)'
+  $version = ([regex]::match($download_page.Content, $versionRegEx) -replace ("Version ", ""))
+
+  $url32 = $url[0]
+
+  $Latest = @{ URL32 = $url32; Version = $version }
+  return $Latest
+}
+
+update -ChecksumFor 32
