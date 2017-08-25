1e63e1f77337f67003290d824b7ddf3d08fb44e9
Journey <timtag1190@gmail.com>
2017-08-25 01:27:29 -0500

    any2icon portable package

---
+++ automatic/any2ico.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'any2ico.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url = 'http://www.carifred.com/quick_any2ico/Quick_Any2Ico.exe'
+$checksum = 'C040F95DF54B5C8EEAEB359640874A6840760A5742EA1105F4CC2197A2AE283A'
+$shortcutName = 'Quick_Any2Ico.lnk'
+$exe = 'Quick_Any2Ico.exe'
+$installerPackage = Join-Path $scriptDir $Exe
+
+$packageArgs = @{
+  packageName  = $packageName
+  fileType     = 'EXE'
+  url          = $url
+  FileFullPath = $installerPackage
+  softwareName = ''
+  checksum     = $checksum
+  checksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
---
+++ automatic/any2ico.portable/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'http://www.carifred.com/quick_any2ico/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "([$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "([$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $regex = '.exe$'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+
+  $versionRegEx = '(\d+)\.(\d+)\.(\d+)\.(\d+)'
+  $version = ([regex]::match($download_page.Content, $versionRegEx))
+
+  $url32 = $releases + $url
+
+  $Latest = @{ URL32 = $url32; version = $version }
+  return $Latest
+}
+
+update -ChecksumFor 32
