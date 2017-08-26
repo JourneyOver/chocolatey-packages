109b5fb60fb428c9afb4397ea229a0ae5d4db956
Journey <timtag1190@gmail.com>
2017-08-26 00:57:57 -0500

    Hopefully fix update script for duckietv prereleases... ????

--- automatic/duckietv_pre/update.ps1
+++ automatic/duckietv_pre/update.ps1
@@ -17,17 +17,32 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $regex = 'x(\d+).zip$'
-  $url = $download_page.links | Where-Object href -match $regex | ForEach-Object href | Select-Object -First 1
+  $url = $download_page.links | Where-Object href -match $regex | ForEach-Object href | Select-Object -First 2
 
-  $bversion = (Split-Path ( Split-Path $url ) -Leaf).Substring(8)
+  $bversion = $url[0] -split 'nightly-|/' | Select-Object -Last 1 -Skip 1
   $version = $bversion -replace '(....(?!$))', '$1.'
   $build = '-nightly'
 
-  $url32 = 'https://github.com' + $url
-  $url64 = 'https://github.com' + $url -replace ('x32', 'x64')
+  $url32 = 'https://github.com' + $url[0]
+  $url64 = 'https://github.com' + $url[1]
 
-  $Latest = @{ PackageName = 'duckietv'; URL32 = $url32; URL64 = $url64; Version = ($version + $build) }
+  $userAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2;)"
+  $32url = "$url32"
+  $64url = "$url64"
+  $path = "C:\DuckieTV-$bversion-windows-x32.zip"
+  $64path = "C:\DuckieTV-$bversion-windows-x64.zip"
+
+  $client = New-Object System.NET.Webclient
+  $client.Headers.Add("user-agent", $userAgent)
+  $client.DownloadFile( $32url, $path )
+  $client.DownloadFile( $64url, $64path )
+  $Latest.Checksum32 = Get-FileHash C:\DuckieTV-$bversion-windows-x32.zip | ForEach-Object Hash
+  $Latest.Checksum64 = Get-FileHash C:\DuckieTV-$bversion-windows-x64.zip | ForEach-Object Hash
+  Remove-Item C:\DuckieTV-$bversion-windows-x32.zip
+  Remove-Item C:\DuckieTV-$bversion-windows-x64.zip
+
+  $Latest = @{ PackageName = 'duckietv'; URL32 = $url32; URL64 = $url64; Version = ($version + $build)}
   return $Latest
 }
 
-update
+update -ChecksumFor none
