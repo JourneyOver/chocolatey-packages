003c2f8832c5d2169ead55cf1d78a145481776df
Journey <timtag1190@gmail.com>
2017-08-26 01:17:22 -0500

    .

--- automatic/duckietv/tools/chocolateyinstall.ps1
+++ automatic/duckietv/tools/chocolateyinstall.ps1
@@ -6,18 +6,19 @@ $checksum32 = 'ECAE9E1DAA64673E72BC06B40908DFCD7DD85D1D0C074350A4049D1E0320060B'
 $checksum64 = '6B3289787E24FDAF4519C6C9A810335C058DD592BA25E6F324E3663FFD8037F2'
 $silentArgs = '/S'
 $validExitCodes = @(0)
-$bits = $ENV:PROCESSOR_ARCHITECTURE -replace ("amd","") -replace ("x86","32")
+$bits = $ENV:PROCESSOR_ARCHITECTURE -replace ("amd", "") -replace ("x86", "32")
 $fileLocation = "$env:ChocolateyInstall\lib\$packageName\tools\DuckieTV-$version-windows-x$bits.exe"
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
-  url           = $url32
-  url64Bit      = $url64
-  checksum      = $checksum32
-  checksum64    = $checksum64
-  checksumType  = 'sha256'
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP'
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
--- automatic/duckietv_pre/tools/chocolateyinstall.ps1
+++ automatic/duckietv_pre/tools/chocolateyinstall.ps1
@@ -6,19 +6,20 @@ $checksum32 = '6AC7E391FECCD2F61C524980594E3CD7A8A2E8198BD2AF210E34A018315C0867'
 $checksum64 = '4A26EDC452AEA2A297DC2A44F2710B873BB22C17E14FA88ED7A62AA045FF8DDE'
 $silentArgs = '/S'
 $validExitCodes = @(0)
-$bits = $ENV:PROCESSOR_ARCHITECTURE -replace ("amd","") -replace ("x86","32")
-$fversion = $version.replace('.','').replace('-nightly','')
+$bits = $ENV:PROCESSOR_ARCHITECTURE -replace ("amd", "") -replace ("x86", "32")
+$fversion = $version.replace('.', '').replace('-nightly', '')
 $fileLocation = "$env:ChocolateyInstall\lib\$packageName\tools\DuckieTV-$fversion-windows-x$bits.exe"
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
-  url           = $url32
-  url64Bit      = $url64
-  checksum      = $checksum32
-  checksum64    = $checksum64
-  checksumType  = 'sha256'
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP'
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
