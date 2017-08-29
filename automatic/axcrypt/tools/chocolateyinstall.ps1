c7d91a0a45489ed9cc35514485fc905e06cb294c
Journey <timtag1190@gmail.com>
2017-08-29 00:01:26 -0500

    First passthrough.
    ~Still got several packages to go through, but this is a first passthrough on at least 2 packages so far.

--- automatic/axcrypt/tools/chocolateyinstall.ps1
+++ automatic/axcrypt/tools/chocolateyinstall.ps1
@@ -4,6 +4,9 @@ $packageName = 'axcrypt'
 $url = 'https://account.axcrypt.net/download/AxCrypt-2-Setup.exe'
 $checksum = 'ff8e1b411cdef0f55ad5a7bfd568891376a58c567024c02201f40b9491279350'
 
+$registryPath = $('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{9E15EF89-8322-C117-CAF2-E79EFAC71395}')
+$version = '2.1.1534'
+
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
@@ -14,4 +17,17 @@ $packageArgs = @{
   checksumType   = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs
+if (Test-Path $registryPath) {
+  $installedVersion = (
+    Get-ItemProperty -Path $registryPath -Name 'DisplayVersion'
+  ).DisplayVersion
+}
+
+if ($installedVersion -match $version) {
+  Write-Output $(
+    "AxCrypt $installedVersion is already installed. " +
+    "Skipping download and installation."
+  )
+} else {
+  Install-ChocolateyPackage @packageArgs
+}
--- automatic/centbrowser_beta/tools/chocolateyuninstall.ps1
+++ automatic/centbrowser_beta/tools/chocolateyuninstall.ps1
@@ -15,17 +15,5 @@ $packageArgs = @{
   File           = $myfile
 }
 
-# Kill CB process before uninstall if running
-$killCB = Get-Process | Where-Object {$_.Path -like "$env:LOCALAPPDATA\CentBrowser\Application\chrome.exe"} -ErrorAction SilentlyContinue
-if ($killCB) {
-  # try gracefully first
-  $killCB.CloseMainWindow()
-  # kill after ten seconds
-  Start-Sleep 10
-  if (!$killCB.HasExited) {
-    $killCB | Stop-Process -Force
-  }
-}
-
 Uninstall-ChocolateyPackage @packageArgs
 # Currently doesn't delete User Data folder
