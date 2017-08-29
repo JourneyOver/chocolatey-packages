a0fbeaca3482d1fb1bcc3110e056a16e858f7a37
Journey <timtag1190@gmail.com>
2017-08-28 21:27:00 -0500

    .

--- manual/ssip.portable/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$packageName = 'ssip.portable'
-$shortcutName = 'Simple Static IP.lnk'
-
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
