e8644aeb74d9479e5a41dd561a434fba6a694341
JourneyOver <timtag1190@gmail.com>
2017-08-30 03:10:34 -0700

    AU: 1 updated - axcrypt [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/7fc483aadc5860b5081b9a8b598cd338df589df8

--- automatic/axcrypt/tools/chocolateyinstall.ps1
+++ automatic/axcrypt/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'axcrypt'
 $url = 'https://account.axcrypt.net/download/AxCrypt-2-Setup.exe'
-$checksum = 'ff8e1b411cdef0f55ad5a7bfd568891376a58c567024c02201f40b9491279350'
+$checksum = '3008e96379f1bb72beaf1aaae333e0977f3ba3073d08986a8e9b290eb8656b58'
 
 $packageArgs = @{
   packageName    = $packageName
