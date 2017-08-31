f7ebff3b9882173faf5055f3970bcff726b10418
JourneyOver <timtag1190@gmail.com>
2017-08-31 11:06:36 -0700

    AU: 1 updated - jackett [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/795e49754cc63bb251c8bfdfcaba18b0d3b0d2c4

--- automatic/jackett/tools/chocolateyInstall.ps1
+++ automatic/jackett/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'jackett'
-$url = 'https://github.com/Jackett/Jackett/releases/download/v0.8.136/Jackett.Installer.Windows.exe'
-$checksum = 'b26651bca7026c7e0309c3dc994f5c52360ad018c5d0ba365585bf8f8ce149e2'
+$url = 'https://github.com/Jackett/Jackett/releases/download/v0.8.140/Jackett.Installer.Windows.exe'
+$checksum = '096c919525a733cc84bbd84c0ccd7a2c300016123393dce3897ce54d637740dc'
 
 $packageArgs = @{
   packageName    = $packageName
