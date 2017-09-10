097a6018fba15320fb929e7cf3d131d15f9c2fd9
Journey <timtag1190@gmail.com>
2017-09-10 10:03:29 -0500

    TVRenamer Package
    ~First package for TVRenamer
    ~Fix process lasso nuspec file to point to correct github folder.

--- automatic/tvrenamer.portable/tools/chocolateyinstall.ps1
+++ automatic/tvrenamer.portable/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'therenamer.portable'
+$packageName = 'tvrenamer.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $url32 = 'https://github.com/tvrenamer/tvrenamer/releases/download/v0.8/TVRenamer-0.8-win32.exe'
 $url64 = 'https://github.com/tvrenamer/tvrenamer/releases/download/v0.8/TVRenamer-0.8-win64.exe'
-$checksum32 = ''
-$checksum64 = ''
-$shortcutName = 'TheRenamer.lnk'
-$exe = '.exe'
+$checksum32 = '2D6CC43EF0E694EAFB54DE47450955E62765DC6620FC55B3FBDCBF891F530FB3'
+$checksum64 = '81F45AE5B676970003B0A102D614FCA8F0AEE9D0E4D6F4D1E555CCA7E6A1E27C'
+$bits = $ENV:PROCESSOR_ARCHITECTURE -replace ("amd", "") -replace ("x86", "32")
+$shortcutName = 'TVRenamer.lnk'
+$exe = "TVRenamer-$version-win$bits.exe"
 $installerPackage = Join-Path $scriptDir $Exe
 
 $packageArgs = @{
---
+++ automatic/tvrenamer.portable/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$releases = 'https://github.com/tvrenamer/tvrenamer/releases'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "([$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "([$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "([$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "([$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $regex = '.exe$'
+  $url = $download_page.links | Where-Object href -match $regex | ForEach-Object href | Select-Object -First 2
+
+  $version = (Split-Path ( Split-Path $url | Select-Object -First 1 ) -Leaf).Substring(1)
+
+  $url32 = 'https://github.com' + $url[0]
+  $url64 = 'https://github.com' + $url[1]
+
+  $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+  return $Latest
+}
+
+update
