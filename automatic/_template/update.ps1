8e4290bd2cd8b4a8c5b82d6d30edc8a706407f96
Journey <timtag1190@gmail.com>
2017-09-12 16:56:40 -0500

    update template

--- automatic/_template/update.ps1
+++ automatic/_template/update.ps1
@@ -1,53 +1,33 @@
-import-module au
-. $PSScriptRoot\..\_scripts\all.ps1
+Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = ''
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-      "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
-      "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-    }
-
-    "$($Latest.PackageName).nuspec" = @{
-      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-    }
-
-    ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-      "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-      "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-      "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
-      "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+      "([$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+      "([$]url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+      "([$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      "([$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
   }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 function global:au_AfterUpdate {
-  Set-DescriptionFromReadme -SkipFirst 2
+  Set-DescriptionFromReadme -SkipFirst 1
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $re = '\.exe$'
-  $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 
-  $version = $url -split '[._-]|.exe' | select -Last 1 -Skip 2
+  $version = $url -split '[._-]|.exe' | Select-Object -Last 1 -Skip 2
 
-  @{
-    Version      = $version
-    URL32        = $url
-    URL64        = $url
-    ReleaseNotes = ''
-  }
+  $latest = @{ URL32 = $url32; URL64 = $url64; Version = $version}
+  return $Latest
 }
 
 update
