ac3c875c6cb19992a91516465c9f05b8da17826d
Journey <timtag1190@gmail.com>
2017-09-16 15:38:33 -0500

    pre-update duckie packages
    ~Just updating duckie packages on the main branch as well, to accomodate a change in update scripts.

--- automatic/duckietv/update.ps1
+++ automatic/duckietv/update.ps1
@@ -1,28 +1,33 @@
-import-module au
+Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://github.com/SchizoDuckie/DuckieTV/releases'
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "([$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "([$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
       "([$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-      "([$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "([$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       "([$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
   }
+}
+
+function global:au_AfterUpdate {
+  Set-DescriptionFromReadme -SkipFirst 1
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $regex = 'x(\d+).zip$'
-  $url = $download_page.links | Where-Object href -match $regex | ForEach-Object href | Select-Object -First 1
+  $url = $download_page.links | Where-Object href -match $regex | ForEach-Object href | Select-Object -First 2
 
-  $version = (Split-Path ( Split-Path $url ) -Leaf)
+  $version = (Split-Path ( Split-Path $url[0] ) -Leaf)
 
-  $url32 = 'https://github.com' + $url
-  $url64 = 'https://github.com' + $url -replace ('x32', 'x64')
+  $url32 = 'https://github.com' + $url[0]
+  $url64 = 'https://github.com' + $url[1]
 
   $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
   return $Latest
--- automatic/duckietv_pre/update.ps1
+++ automatic/duckietv_pre/update.ps1
@@ -1,16 +1,21 @@
-import-module au
+Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://github.com/DuckieTV/Nightlies/releases'
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "([$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "([$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
       "([$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-      "([$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "([$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       "([$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
   }
+}
+
+function global:au_AfterUpdate {
+  Set-DescriptionFromReadme -SkipFirst 1
 }
 
 function global:au_GetLatest {
@@ -25,21 +30,6 @@ function global:au_GetLatest {
 
   $url32 = 'https://github.com' + $url[0]
   $url64 = 'https://github.com' + $url[1]
-
-  $userAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2;)"
-  $32url = "$url32"
-  $64url = "$url64"
-  $path = "C:\DuckieTV-$bversion-windows-x32.zip"
-  $64path = "C:\DuckieTV-$bversion-windows-x64.zip"
-
-  $client = New-Object System.NET.Webclient
-  $client.Headers.Add("user-agent", $userAgent)
-  $client.DownloadFile( $32url, $path )
-  $client.DownloadFile( $64url, $64path )
-  $Latest.Checksum32 = Get-FileHash C:\DuckieTV-$bversion-windows-x32.zip | ForEach-Object Hash
-  $Latest.Checksum64 = Get-FileHash C:\DuckieTV-$bversion-windows-x64.zip | ForEach-Object Hash
-  Remove-Item C:\DuckieTV-$bversion-windows-x32.zip
-  Remove-Item C:\DuckieTV-$bversion-windows-x64.zip
 
   $Latest = @{ PackageName = 'duckietv'; URL32 = $url32; URL64 = $url64; Version = ($version + $build)}
   return $Latest
