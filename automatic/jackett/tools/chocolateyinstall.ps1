44ef24f160a5e42dc4cf0828394add84b50e1d9e
JourneyOver <timtag1190@gmail.com>
2017-10-03 11:00:09 -0700

    AU: 1 updated - jackett [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/8ad98f480a9dd43ffd14e2bb92437bfc91a82e90

--- automatic/jackett/tools/chocolateyinstall.ps1
+++ automatic/jackett/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'jackett'
-$url = 'https://github.com/Jackett/Jackett/releases/download/v0.8.246/Jackett.Installer.Windows.exe'
-$checksum = '4252a5d0259fb9dd9ed5e0f917a597de1d15276bb671a13c1ef78b8fa356083a'
+$url = 'https://github.com/Jackett/Jackett/releases/download/v0.8.249/Jackett.Installer.Windows.exe'
+$checksum = '8e397421f2a2d0744f0268effb0ff39ee7c24a80c7ddd5c61133434844c9cb1d'
 $registrypaths = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{C2A9FC00-AA48-4F17-9A72-62FBCEE2785B}_is1', 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{C2A9FC00-AA48-4F17-9A72-62FBCEE2785B}_is1')
-$version = '0.8.246'
+$version = '0.8.249'
 
 $packageArgs = @{
   packageName    = $packageName
