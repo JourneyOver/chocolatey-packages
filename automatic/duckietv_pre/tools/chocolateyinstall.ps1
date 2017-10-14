917cd3a0ae86a454e82dd1e06dab3553cd28238d
JourneyOver <timtag1190@gmail.com>
2017-10-13 17:07:00 -0700

    AU: 1 updated - duckietv [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/b48f9463154d711c580f56e3fd62646ae707bde6

--- automatic/duckietv_pre/tools/chocolateyinstall.ps1
+++ automatic/duckietv_pre/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = 'duckietv'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-201710130130/DuckieTV-201710130130-windows-x32.zip'
-$url64 = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-201710130130/DuckieTV-201710130130-windows-x64.zip'
-$checksum = 'cd3a3c70204e8868f5bbcaa424c9bf6bd4ed71a8a4aeda8f172746858fff962a'
-$checksum64 = 'a333862172e4b3e1e79abf9d5b868809de2bd118479fef71bad9504fef5768ed'
+$url = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-201710140130/DuckieTV-201710140130-windows-x32.zip'
+$url64 = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-201710140130/DuckieTV-201710140130-windows-x64.zip'
+$checksum = '3ec56bebb51ffebd67be67f7a0aba03bfd282811d058806f888db2d4c9648c58'
+$checksum64 = 'ffef2f5bed99b06c5d4d16e0feca660844d2525aa34f614bf67399692e782f3b'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 $bits = Get-ProcessorBits
