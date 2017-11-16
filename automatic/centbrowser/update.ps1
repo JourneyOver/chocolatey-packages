fbbb81b3949fe92d538719fcda1c90e4e246e1fe
Journey <timtag1190@gmail.com>
2017-11-16 13:46:41 -0600

    update 3 of ??
    ~Delete Centbrowser_beta package folder in favour of doing both non-beta and beta in the same update script/package.
    Sadly I don't think I can do this with my duckietv packages because of how I set up the numbering sequence for nightly releases :/

--- automatic/centbrowser/update.ps1
+++ automatic/centbrowser/update.ps1
@@ -3,6 +3,9 @@ Import-Module au
 $releases_32 = 'http://static.centbrowser.com/installer_32/'
 $releases_64 = 'http://static.centbrowser.com/installer_64/'
 
+$breleases_32 = 'http://static.centbrowser.com/beta_32/'
+$breleases_64 = 'http://static.centbrowser.com/beta_64/'
+
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
@@ -17,23 +20,42 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page_32 = Invoke-WebRequest -Uri $releases_32 -UseBasicParsing
   $download_page_64 = Invoke-WebRequest -Uri $releases_64 -UseBasicParsing
+  $bdownload_page_32 = Invoke-WebRequest -Uri $breleases_32 -UseBasicParsing
+  $bdownload_page_64 = Invoke-WebRequest -Uri $breleases_64 -UseBasicParsing
 
   $regex = '.exe$'
+
   $url_32 = $download_page_32.links | Where-Object href -match $regex | Select-Object -Last 2 -expand href
   $url_64 = $download_page_64.links | Where-Object href -match $regex | Select-Object -Last 2 -expand href
-
   $version = $url_32[0] -split 'centbrowser_|.exe' | Select-Object -Last 1 -Skip 1
   $version64 = $url_64[0] -split 'centbrowser_|_x64.exe' | Select-Object -Last 1 -Skip 1
 
   if ($version -ne $version64) {
-    throw "32-bit and 64-bit version do not match. Please investigate."
+    throw "32-bit and 64-bit versions do not match. Please investigate."
+  }
+
+  $burl_32 = $bdownload_page_32.links | Where-Object href -match $regex | Select-Object -Last 2 -expand href
+  $burl_64 = $bdownload_page_64.links | Where-Object href -match $regex | Select-Object -Last 2 -expand href
+  $bversion = $burl_32[0] -split 'centbrowser_|.exe' | Select-Object -Last 1 -Skip 1
+  $bversion64 = $burl_64[0] -split 'centbrowser_|_x64.exe' | Select-Object -Last 1 -Skip 1
+  $build = "-beta"
+
+  if ($bversion -ne $bversion64) {
+    throw "32-bit and 64-bit beta versions do not match. Please investigate."
   }
 
   $url32 = $releases_32 + $url_32[0]
   $url64 = $releases_64 + $url_64[0]
+  $burl32 = $breleases_32 + $burl_32[0]
+  $burl64 = $breleases_64 + $burl_64[0]
 
-  $Latest = @{ PackageName = 'CentBrowser'; URL32 = $url32; URL64 = $url64; Version = $version }
-  return $Latest
+  if ($bversion -gt $version) {
+    $Latest = @{ PackageName = 'CentBrowser'; URL32 = $burl32; URL64 = $burl64; Version = ($bversion + $build)  }
+    return $Latest
+  } else {
+    $Latest = @{ PackageName = 'CentBrowser'; URL32 = $url32; URL64 = $url64; Version = $version  }
+    return $Latest
+  }
 }
 
 update
