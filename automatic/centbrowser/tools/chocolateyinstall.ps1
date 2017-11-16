61201fff243baf389d0289a3cd0bff9e25566ddb
JourneyOver <timtag1190@gmail.com>
2017-11-16 06:10:22 +0000

    AU: 2 updated - CentBrowser duckietv [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/69658684b036c998367bbfce09985b605c4fb4fd

--- automatic/centbrowser/tools/chocolateyinstall.ps1
+++ automatic/centbrowser/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $pp = Get-PackageParameters
 
 $packageName = 'CentBrowser'
-$url = 'http://static.centbrowser.com/installer_32/centbrowser_3.0.4.27.exe'
-$url64 = 'http://static.centbrowser.com/installer_64/centbrowser_3.0.4.27_x64.exe'
-$checksum = 'b25841ccfcbde4f37339cce0c3873454a1a9d8859fc2d01c059c526558ae6209'
-$checksum64 = '90864dabdb00620851f924cb4264681e4c9287c87e1fe0af5eb2e8db57f0e56e'
+$url = 'http://static.centbrowser.com/installer_32/centbrowser_3.0.4.29.exe'
+$url64 = 'http://static.centbrowser.com/installer_64/centbrowser_3.0.4.29_x64.exe'
+$checksum = 'a1d8c30dec658c239e363bb141f19166164b725dce9a8bae71f8f2dcb105f869'
+$checksum64 = '16951522693b3c03cbc70b707492d1b15dfc789e9e49defc64eabcfea7fc5123'
 
 if (!$pp['dir']) { $pp['dir'] = "$env:LOCALAPPDATA" }
 
