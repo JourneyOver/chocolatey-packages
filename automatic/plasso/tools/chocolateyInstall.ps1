0f8017b539c799b8c6a5e17cce12eca4fb463bc1
JourneyOver <timtag1190@gmail.com>
2018-01-09 00:10:24 +0000

    AU: 1 updated - plasso [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/8107748e25924c99a46c4a89fdb06e9f627846a4

--- automatic/plasso/tools/chocolateyInstall.ps1
+++ automatic/plasso/tools/chocolateyInstall.ps1
@@ -5,15 +5,15 @@ $pp = Get-PackageParameters
 $packageName = 'plasso'
 $url = 'https://bitsum.com/files/processlassosetup32.exe'
 $url64 = 'https://bitsum.com/files/processlassosetup64.exe'
-$checksum = 'caf1204fee70129366e3eba6dbe7ea97c6b7fe64665fab0b60850007564f8aae'
-$checksum64 = '16610d66d53053d9eaf74f145cf31a6800f87e37609f03b2f15ce599f7ec7910'
+$checksum = '57588a44f073881ba7f1552e1109eeff2ff5533cbcd22cf1d96e698c8468c86d'
+$checksum64 = '5f90720301ebadac71699c9d5ef6232df8dd95e135ac34da54b6cc9e8780633f'
 
 $checksumType = 'sha256'
 
 $surl = 'https://bitsum.com/files/server/processlassosetup32.exe'
 $surl64 = 'https://bitsum.com/files/server/processlassosetup64.exe'
-$schecksum = 'febbffde7582cbee01a7d6a5cf91d70158bc613c106f91296861de50f831ca38'
-$schecksum64 = '239d5adf3df3024d8d121c40aa2530a4bf9e3034f13c89aba2df69f48ed49313'
+$schecksum = 'b561335731c6db2e85b9ee145a8beda64ca941eced3444fb02ba217abc79e323'
+$schecksum64 = '76d39042774c513ce6bd25e47b66fc572e60b942b0a9e50312c2b6c2730b3d90'
 
 if (!$pp['language']) { $pp['language'] = 'English' }
 if (!$pp['gui_start_type']) { $pp['gui_start_type'] = 'all,uac' }
