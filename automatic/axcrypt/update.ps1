f18afc3de054ab17c336f388a01df1273d3af3f5
Journey <timtag1190@gmail.com>
2018-06-15 09:42:04 -0500

    Manual Update
    Axcrypt's site is being weird, so update script is broken for now...

--- automatic/axcrypt/update.ps1
+++ automatic/axcrypt/update.ps1
@@ -1,6 +1,8 @@
 Import-Module au
 
-$releases = 'http://www.axantum.com/AxCrypt/Downloads.html'
+$releases = 'https://www.axcrypt.net/download/'
+$downloadraw_url = 'https://account.axcrypt.net/download/'
+$versionnum = 'https://www.axcrypt.net/cryptographic-hashes-files/'
 
 function global:au_SearchReplace {
   @{
@@ -18,14 +20,15 @@ function global:au_BeforeUpdate {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $version_page = Invoke-WebRequest -Uri $versionnum -UseBasicParsing
 
-  $regex = 'AxCrypt-'
-  $url = $download_page.links | Where-Object href -Match $regex | Select-Object -First 1 -Expand href
+  $regex = 'AxCrypt-(\d+)-Setup.exe'
+  $url = ([regex]::match($download_page.Content, $regex))
 
-  $versionRegEx = 'AxCrypt-2\.(\d+)\.(\d+)\.(\d+)'
-  $version = ([regex]::match($download_page.Content, $versionRegEx)) -replace ('AxCrypt-', '')
+  $versionRegEx = '(\d+).(\d+).(\d+).(\d+)-Setup.exe'
+  $version = ([regex]::match($version_page.Content, $versionRegEx) -replace ('-Setup.exe', ''))
 
-  $url32 = $url;
+  $url32 = $downloadraw_url + $url
 
   $Latest = @{ URL32 = $url32; Version = $version }
   return $Latest
