1c26d4efe38dd583764d5046dbe896c76b96a2da
Journey <timtag1190@gmail.com>
2018-06-15 08:59:51 -0500

    Revert "Update update.ps1"
    This reverts commit 1f77a5c89255cb92f10ea957c28c69da6527b180.

--- automatic/axcrypt/update.ps1
+++ automatic/axcrypt/update.ps1
@@ -1,8 +1,6 @@
 Import-Module au
 
-# Parse Softpedia for version number for now until I can figure something better out 
-# since 'https://www.axcrypt.net/information/release-notes/' doesn't seem to want to work anymore.
-$versionnum = 'http://www.softpedia.com/get/Security/Encrypting/AxCrypt.shtml'
+$releases = 'http://www.axantum.com/AxCrypt/Downloads.html'
 
 function global:au_SearchReplace {
   @{
@@ -19,13 +17,15 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-  $version_page = Invoke-WebRequest -Uri $versionnum -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $versionRegEx = '(\d+).(\d+).(\d+).(\d+)-Setup.exe'
-  $version = ([regex]::match($version_page.Content, $versionRegEx)) -replace ('-Setup.exe', '')
+  $regex = 'AxCrypt-'
+  $url = $download_page.links | Where-Object href -Match $regex | Select-Object -First 1 -Expand href
 
-  # Url Never Changes it seems so hardcode it for now.
-  $url32 = 'https://account.axcrypt.net/download/axcrypt-2-setup.exe';
+  $versionRegEx = 'AxCrypt-2\.(\d+)\.(\d+)\.(\d+)'
+  $version = ([regex]::match($download_page.Content, $versionRegEx)) -replace ('AxCrypt-', '')
+
+  $url32 = $url;
 
   $Latest = @{ URL32 = $url32; Version = $version }
   return $Latest
