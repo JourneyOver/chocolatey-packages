ac0c0098ac87917e11f8efc89f1050a9f428243e
JourneyOver <timtag1190@gmail.com>
2018-07-21 06:15:28 +0000

    AU: 2 updated - CentBrowser lidarr [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/cc04f849c13082d4309ed0ee9c6e791bdc2649bc

--- automatic/centbrowser/tools/chocolateyInstall.ps1
+++ automatic/centbrowser/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $pp = Get-PackageParametersBuiltIn
 
 $packageName = 'CentBrowser'
-$url = 'http://static.centbrowser.com/beta_32/centbrowser_3.5.3.25.exe'
-$url64 = 'http://static.centbrowser.com/beta_64/centbrowser_3.5.3.25_x64.exe'
-$checksum = '05753bce5ee7132e28cc37fcebe19f092d8ae79ff450552974df2e398c52955c'
-$checksum64 = 'c1f0aa91d897fd74fd1652580871d974856153e48746e5e80a4b2e97fc0ef3cd'
+$url = 'http://static.centbrowser.com/installer_32/centbrowser_3.5.3.39.exe'
+$url64 = 'http://static.centbrowser.com/installer_64/centbrowser_3.5.3.39_x64.exe'
+$checksum = '62922a8de59d284bb60078c7342715407b0540c0a58c9c6a62b2b7ec11353dff'
+$checksum64 = '0d51dcf1dde625427fdb47a9e97fc8d71656971e348a98337a734b69f171e052'
 $checksumType = 'sha256'
 
 if (!$pp['dir']) { $pp['dir'] = "$env:LOCALAPPDATA" }
