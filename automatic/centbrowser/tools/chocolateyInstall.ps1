af055720f3bb6301003cc89bbf349347605b53eb
JourneyOver <timtag1190@gmail.com>
2018-10-29 13:45:41 +0000

    AU: 2 updated - CentBrowser metropolislauncher [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/4e86620755938a5c1696e8b6b4dec421eda49fe1

--- automatic/centbrowser/tools/chocolateyInstall.ps1
+++ automatic/centbrowser/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageName = 'CentBrowser'
 $url = 'http://static.centbrowser.com/installer_32/centbrowser_3.6.8.96.exe'
 $url64 = 'http://static.centbrowser.com/installer_64/centbrowser_3.6.8.96_x64.exe'
 $checksum = '655ad26403544be86ea5aaedf8c27405737042043e4b8bd4a953860239fd667d'
-$checksum64 = 'd42357b5ff63361b90a490db64d36496338ec211cd8af9de14c9ae8fbb64a42d'
+$checksum64 = '2fcb5d9ccc8992c678d1a3b5ddb3ad83a8350ece19dbb41e87052071f6544478'
 $checksumType = 'sha256'
 
 if (!$pp['dir']) { $pp['dir'] = "$env:LOCALAPPDATA" }
