ce91f6c9cbe2f0afc3f98da6c468240d4fb0f5b0
Journey <timtag1190@gmail.com>
2019-01-25 20:25:05 -0600

    Fix axcrypt expectedembeddedmatch
    Should of been lowercased, at least so far testing seems to actually work though which is great!
    
    Will probably start actual work on add package tests to all packages tomorrow.. as it's kinda getting to late to really do a whole lot more tonight.

--- automatic/axcrypt/package.Tests.ps1
+++ automatic/axcrypt/package.Tests.ps1
@@ -3,6 +3,6 @@
 Run-PesterTests `
   -packageName "axcrypt" `
   -packagePath "$PSScriptRoot" `
-  -expectedEmbeddedMatch '^AxCrypt-[\d\.]+-Setup\.exe$' `
+  -expectedEmbeddedMatch '^axcrypt-[\d\.]+-setup\.exe$' `
   -licenseShouldMatch 'GNU GENERAL PUBLIC LICENSE' `
   -expectedDefaultDirectory "${env:ProgramData}\Package Cache\{4802bd28-932d-4070-99e2-068ea74d872d}"
