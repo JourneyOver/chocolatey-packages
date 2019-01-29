9a03646f15938fd7b9bfd42f0399453729888089
JourneyOver <timtag1190@gmail.com>
2019-01-29 18:03:03 +0000

    AU: 1 updated - gitahead [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/348496400370115510973d44fee637e94daaae9c

--- automatic/gitahead/tools/chocolateyInstall.ps1
+++ automatic/gitahead/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'gitahead'
-$url = ''
-$url64 = ''
-$checksum = ''
-$checksum64 = ''
+$url = 'https://github.com/gitahead/gitahead/releases/download/v2.5.3/GitAhead-win32-2.5.3.exe'
+$url64 = 'https://github.com/gitahead/gitahead/releases/download/v2.5.3/GitAhead-win64-2.5.3.exe'
+$checksum = 'f95ad6c6ec2f7b908e3a33a13ec80556ad9822210b603ae34a09667165345404'
+$checksum64 = 'da50ca5f2a7f75575c1efbf2f6e90daa6b049e26b3be3bf6d6eda4eea253cc22'
 $checksumType = 'sha256'
 
 $packageArgs = @{
