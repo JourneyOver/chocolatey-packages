16c17772d5bcfdc48443d57da3637655e3601e70
Journey <timtag1190@gmail.com>
2019-01-30 11:24:59 -0600

    rembed this package

--- automatic/gitahead/tools/chocolateyInstall.ps1
+++ automatic/gitahead/tools/chocolateyInstall.ps1
@@ -1,23 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'gitahead'
-$url = 'https://github.com/gitahead/gitahead/releases/download/v2.5.3/GitAhead-win32-2.5.3.exe'
-$url64 = 'https://github.com/gitahead/gitahead/releases/download/v2.5.3/GitAhead-win64-2.5.3.exe'
-$checksum = 'f95ad6c6ec2f7b908e3a33a13ec80556ad9822210b603ae34a09667165345404'
-$checksum64 = 'da50ca5f2a7f75575c1efbf2f6e90daa6b049e26b3be3bf6d6eda4eea253cc22'
-$checksumType = 'sha256'
+
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+$fileLocation = Get-Item "$toolsDir\GitAhead-win32-*"
+$fileLocation64 = Get-Item "$toolsDir\GitAhead-win64-*"
 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = $url
-  url64Bit       = $url64
+  file           = $fileLocation
+  file64         = $fileLocation64
   silentArgs     = "/S"
   validExitCodes = @(0)
-  checksum       = $checksum
-  checksum64     = $checksum64
-  checksumType   = $checksumType
-  checksumType64 = $checksumType
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+# Remove the installers as there is no more need for it
+Remove-Item $toolsDir\*.exe -ea 0 -Force
--- automatic/gitahead/update.ps1
+++ automatic/gitahead/update.ps1
@@ -6,14 +6,27 @@ $repoName = "gitahead"
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*[$]url(32)?\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*[$]url64\s*=\s*)('.*')"         = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*[$]checksum(32)?\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*[$]checksum64\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*[$]checksumType\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType32)'"
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUri)>"
+      "(?i)(^\s*url(32)?\:\s*).*"         = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*url64?\:\s*).*"           = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum(32)?\:\s*).*"    = "`${1}$($Latest.Checksum32)"
+      "(?i)(^\s*checksum64?\:\s*).*"      = "`${1}$($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:\s*).*"  = "`${1}$($Latest.ChecksumType32)"
     }
   }
+}
+
+function global:au_BeforeUpdate($Package) {
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { Remove-Item -Force $licenseFile }
+
+  Invoke-WebRequest -UseBasicParsing -Uri $($Package.nuspecXml.package.metadata.licenseUrl -replace 'blob', 'raw') -OutFile $licenseFile
+  if (!(Get-ValidOpenSourceLicense -path "$licenseFile")) {
+    throw "Unknown license download. Please verify it still contains distribution rights."
+  }
+
+  Get-RemoteFiles -Purge -NoSuffix
 }
 
 function global:au_GetLatest {
@@ -22,8 +35,8 @@ function global:au_GetLatest {
   $url32 = $release.latestStable.Assets | Where-Object { $_ -match 'win32-(\d+)\.(\d+)\.(\d+)\.exe$' } | Select-Object -First 1
   $url64 = $release.latestStable.Assets | Where-Object { $_ -match 'win64-(\d+)\.(\d+)\.(\d+)\.exe$' } | Select-Object -First 1
 
-  $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $release.latestStable.Version }
+  $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $release.latestStable.Version; ReleaseUri = $release.latestStable.ReleaseUrl }
   return $Latest
 }
 
-update
+update -ChecksumFor none
