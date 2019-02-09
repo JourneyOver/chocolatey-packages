4d100678ded321d4a5b9c354d34537b38408dffc
Journey <timtag1190@gmail.com>
2019-02-09 14:27:13 -0600

    Package tests
    Check to see if duckienightly test will remove checksum.txt file so it can run test on nightly.
    Set up simple unit testing for gitahead

--- automatic/duckietv/package.Tests.ps1
+++ automatic/duckietv/package.Tests.ps1
@@ -1,5 +1,11 @@
 . "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
 
+#Remove hashcheck.txt file to run streams test
+$FileName = "$PSScriptRoot\hashcheck.txt"
+if (Test-Path $FileName) {
+  Remove-Item $FileName
+}
+
 Run-PesterTests `
   -packageName "duckietv" `
   -packagePath "$PSScriptRoot" `
---
+++ automatic/gitahead/package.Tests.ps1
@@ -0,0 +1,9 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+Run-PesterTests `
+  -packageName "gitahead" `
+  -packagePath "$PSScriptRoot" `
+  -expectedEmbeddedMatch '^GitAhead-win(32|64)-[\d\.]+\.exe$' `
+  -licenseShouldMatch 'MIT License' `
+  -expectedDefaultDirectory "${env:ProgramFiles}\GitAhead"
+  -test32bit
