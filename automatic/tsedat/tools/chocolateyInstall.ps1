f6870af9967a973f5384f042255b5e9a088ccf30
JourneyOver <timtag1190@gmail.com>
2019-02-21 00:07:43 +0000

    AU: 2 updated - sonarr tsedat [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/e373775078c02358eb12fe79d9497319e93a0969

--- automatic/tsedat/tools/chocolateyInstall.ps1
+++ automatic/tsedat/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'tsedat'
-$url = 'http://www.sequencepublishing.website/_files/TheSage_Setup_7-26-2684.exe'
-$checksum = '10c17d6255499c47b67db108f84b328373404579b95aeba798468fe6c0e307f8'
+$url = 'http://www.sequencepublishing.website/_files/TheSage_Setup_7-28-2686.exe'
+$checksum = 'c365022420ff82b47c1a90bf64680b8cd6ee23ae5ed28f8ebaa8bf689f9322b8'
 $checksumType = 'sha256'
 
 $packageArgs = @{
