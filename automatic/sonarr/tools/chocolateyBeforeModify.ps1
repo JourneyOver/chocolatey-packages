9980b263657b4b1df48d6776a70b712e251a2cfb
JourneyOver <timtag1190@gmail.com>
2019-03-04 00:07:39 +0000

    AU: 2 updated - jackett sonarr [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/6f5f8d991a49dca86b01cf85c1f5cb0481a9134c

--- automatic/sonarr/tools/chocolateyBeforeModify.ps1
+++ automatic/sonarr/tools/chocolateyBeforeModify.ps1
@@ -1,5 +1,5 @@
 # Stop Sonarr service before upgrade/uninstall if running
-$service = 'Sonarr'
+$service = 'NzbDrone'
 if (Get-Service "$service" -ErrorAction SilentlyContinue) {
   $running = Get-Service $service
   if ($running.Status -eq "Running") {
