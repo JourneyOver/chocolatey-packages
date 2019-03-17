2035115af97f85b89e4edc23058e8a2c1567eec0
Journey <timtag1190@gmail.com>
2019-03-17 02:57:55 -0500

    meh
    Just do stable and beta version of sonarr, testing phantom stream is being weird right now...

--- automatic/sonarr/package.Tests.ps1
+++ automatic/sonarr/package.Tests.ps1
@@ -3,9 +3,9 @@
 $packageName = Split-Path -Leaf $PSScriptRoot
 
 Run-PesterTests `
-    -packageName "$packageName" `
-    -packagePath "$PSScriptRoot" `
-    -streams "phantom" `
-    -expectedEmbeddedMatch '^Sonarr\.phantom-develop\.[\d\.]+\.windows\.exe$' `
-    -licenseShouldMatch 'GNU GENERAL PUBLIC LICENSE' `
-    -expectedDefaultDirectory "${env:ProgramData}\Sonarr"
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -streams "stable", "beta" `
+  -expectedEmbeddedMatch '^NzbDrone\.(master|develop)\.exe$' `
+  -licenseShouldMatch 'GNU GENERAL PUBLIC LICENSE' `
+  -expectedDefaultDirectory "${env:ProgramData}\NzbDrone"
