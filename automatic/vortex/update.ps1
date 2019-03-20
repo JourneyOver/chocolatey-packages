0324cbe63ccea56d1fed6a00a26700a682f637c9
Journey <timtag1190@gmail.com>
2019-03-20 17:39:26 -0500

    Vortex Package (#36)
    * images
    
    * vortex package
    
    * Fix uninstall

---
+++ automatic/vortex/update.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$repoUser = "Nexus-Mods"
+$repoName = "Vortex"
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUri)>"
+      "(?i)(^\s*url(32)?\:\s*).*"         = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum(32)?\:\s*).*"    = "`${1}$($Latest.Checksum32)"
+      "(?i)(^\s*checksum\s*type\:\s*).*"  = "`${1}$($Latest.ChecksumType32)"
+    }
+  }
+}
+
+function global:au_BeforeUpdate($Package) {
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { Remove-Item -Force $licenseFile }
+
+  Invoke-WebRequest -UseBasicParsing -Uri $($Package.nuspecXml.package.metadata.licenseUrl -replace 'blob', 'raw') -OutFile $licenseFile
+  if (!(Get-ValidOpenSourceLicense -path "$licenseFile")) {
+    throw "Unknown license download. Please verify it still contains distribution rights."
+  }
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_GetLatest {
+  $release = Get-LatestGithubReleases $repoUser $repoName $true
+
+  $url32 = $release.latest.Assets | Where-Object { $_ -match '\.exe$' } | Select-Object -First 1
+
+  $Latest = @{ URL32 = $url32; Version = $release.latest.Version; ReleaseUri = $release.latest.ReleaseUrl }
+  return $Latest
+}
+
+update -ChecksumFor none
