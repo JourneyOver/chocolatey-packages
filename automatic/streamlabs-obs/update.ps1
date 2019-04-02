8610db63e3d11a87b80429b31d0cd7918a035684
Journey <timtag1190@gmail.com>
2019-04-02 10:39:09 -0500

    [AU streamlabs-obs:0.12.3]
    Reembed streamlabs-obs package

--- automatic/streamlabs-obs/update.ps1
+++ automatic/streamlabs-obs/update.ps1
@@ -1,15 +1,28 @@
 ﻿Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://streamlabs.com/slobs/download'
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*[$]url(64)?\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*[$]checksum(64)?\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*[$]checksumType\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType64)'"
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*url(64)?\:\s*).*"        = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum(64)?\:\s*).*"   = "`${1}$($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:\s*).*" = "`${1}$($Latest.ChecksumType64)"
     }
   }
+}
+
+function global:au_BeforeUpdate($Package) {
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { Remove-Item -Force $licenseFile }
+
+  Invoke-WebRequest -UseBasicParsing -Uri $($Package.nuspecXml.package.metadata.licenseUrl -replace 'blob', 'raw') -OutFile $licenseFile
+  if (!(Get-ValidOpenSourceLicense -path "$licenseFile")) {
+    throw "Unknown license download. Please verify it still contains distribution rights."
+  }
+
+  Get-RemoteFiles -Purge -NoSuffix
 }
 
 function global:au_GetLatest {
@@ -22,4 +35,4 @@ function global:au_GetLatest {
   return $Latest
 }
 
-update -ChecksumFor 64
+update -ChecksumFor none
