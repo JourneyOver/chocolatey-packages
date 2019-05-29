fd3e32e991d705e743bd0f04da0f0f1b94c2a3c0
JourneyOver <timtag1190@gmail.com>
2019-05-29 18:07:49 +0000

    AU: 2 updated - streamlabs-obs vortex [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/27868a53bcb12d57585231e4b4b59734e7c0ed77

--- automatic/streamlabs-obs/tools/chocolateyInstall.ps1
+++ automatic/streamlabs-obs/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'streamlabs-obs'
-$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+OBS+Setup+0.14.1.exe'
-$checksum64 = '44cdc6420473a01220d34bf733550e1074b86a6c417b726d728e058c0896fa42'
+$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+OBS+Setup+0.15.0.exe'
+$checksum64 = 'a90a2236ff0e46110bcff78790dcd87718054a4398bea94270afb2bc0efe9cad'
 $checksumType = 'sha256'
 
 if ((Get-OSArchitectureWidth 32) -or $env:ChocolateyForceX86) {
