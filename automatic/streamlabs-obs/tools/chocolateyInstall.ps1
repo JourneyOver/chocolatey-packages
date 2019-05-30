be1952d0864f29d23d3c544063ba99ebfcc1ec92
JourneyOver <timtag1190@gmail.com>
2019-05-30 00:08:33 +0000

    AU: 3 updated - jackett sonarr streamlabs-obs [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/e7495cec3b4c50ee6805921a75faab29315e9e65

--- automatic/streamlabs-obs/tools/chocolateyInstall.ps1
+++ automatic/streamlabs-obs/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'streamlabs-obs'
-$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+OBS+Setup+0.15.0.exe'
-$checksum64 = 'a90a2236ff0e46110bcff78790dcd87718054a4398bea94270afb2bc0efe9cad'
+$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+OBS+Setup+0.15.1.exe'
+$checksum64 = 'd21ccca35dcbc67b9e13632c79c9d86bbc16a11e6b126bda63762d6265eaeafb'
 $checksumType = 'sha256'
 
 if ((Get-OSArchitectureWidth 32) -or $env:ChocolateyForceX86) {
