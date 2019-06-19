9606ba78ab949619ee8d6926160243d086e64b08
Journey <timtag1190@gmail.com>
2019-06-19 16:50:28 -0500

    This should work?

--- automatic/renamemytvseries2/update.ps1
+++ automatic/renamemytvseries2/update.ps1
@@ -16,11 +16,23 @@ function global:au_GetLatest {
   $download = Invoke-WebRequest -Uri $releases -UseBasicParsing -Headers @{ "Accept-Encoding" = 'gzip' }
 
   $versionRegEx = '(\d+).(\d+).(\d+).+?-Windows-32bit-setup.exe'
-  $version = ([regex]::match($download.Content, $versionRegEx) -replace ('-Windows-32bit-setup.exe', '') -replace ('b', '-beta'))
+  $version = ([regex]::match($download.Content, $versionRegEx) -replace ('-Windows-32bit-setup.exe', ''))
 
   $url32 = 'https://www.tweaking4all.com/?wpfb_dl=148'
 
-  $Latest = @{ URL32 = $url32; Version = $version }
+  # Check checksum of url to determine if checksums are out of date.
+  $current_checksum = (Get-Item ".\tools\chocolateyInstall.ps1" | Select-String '^[$]checksum\b') -split "=|'" | Select-Object -Last 1 -Skip 1
+  if ($current_checksum.Length -ne 64) { throw "Can't find current checksum" }
+  $remote_checksum = Get-RemoteChecksum $url32
+  $Notation = Get-Date -UFormat "%Y%m%d"
+  if ($current_checksum -ne $remote_checksum) {
+    Write-Host 'Remote checksum is different then the current one, forcing update'
+    $version = $version -replace ('b', ".$Notation")
+    $global:au_old_force = $global:au_force
+    $global:au_force = $true
+  }
+
+  $Latest = @{ URL32 = $url32; Version = $version -replace ('b', '') }
   return $Latest
 }
 
