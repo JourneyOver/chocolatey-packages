cd06b9b8e56e0be848c5217916c41173a93d92e1
Journey <timtag1190@gmail.com>
2019-07-09 05:11:15 -0500

    Hopefully this fixes it?

--- automatic/tsedat/tools/chocolateyUninstall.ps1
+++ automatic/tsedat/tools/chocolateyUninstall.ps1
@@ -20,7 +20,8 @@ Uninstall-ChocolateyPackage @packageArgs
 $fexist = Test-Path "${env:LOCALAPPDATA}\TheSage"
 if ($fexist) {
   Write-Host "Removing TheSage Folder that's left behind"
-  Remove-Item "${env:LOCALAPPDATA}\TheeSage" -Recurse -Force
+  Start-Sleep -Seconds 5
+  Remove-Item "${env:LOCALAPPDATA}\TheSage" -Recurse -Force
 } else {
   Write-Host "TheSage Folder not found"
 }
