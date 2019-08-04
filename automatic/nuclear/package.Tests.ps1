63cc143077d5ed113f9526961ac35934a203724e
Journey Over <timtag1190@gmail.com>
2019-08-04 08:33:04 -0500

    Fix for nuclear package tests

--- automatic/nuclear/package.Tests.ps1
+++ automatic/nuclear/package.Tests.ps1
@@ -1,10 +1,10 @@
-. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+﻿. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
 
 $packageName = Split-Path -Leaf $PSScriptRoot
 
 Run-PesterTests `
   -packageName "$packageName" `
   -packagePath "$PSScriptRoot" `
-  -expectedEmbeddedMatch '^nuclear\.Setup\.[\d\.]+\.exe$' `
+  -expectedEmbeddedMatch '(?i)^nuclear(\.|\-)Setup(\.|\-)[\d\.]+\.exe$' `
   -licenseShouldMatch 'GNU GENERAL PUBLIC LICENSE' `
   -expectedDefaultDirectory "${env:LOCALAPPDATA}\Programs\nuclear"
