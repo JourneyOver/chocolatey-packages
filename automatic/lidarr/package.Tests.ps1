7f445e38bca5c04fc25a012ac62ba8ccdd1ac7af
Journey <timtag1190@gmail.com>
2019-09-27 12:13:29 -0500

    Update package.Tests.ps1

--- automatic/lidarr/package.Tests.ps1
+++ automatic/lidarr/package.Tests.ps1
@@ -5,6 +5,6 @@ $packageName = Split-Path -Leaf $PSScriptRoot
 Run-PesterTests `
   -packageName "$packageName" `
   -packagePath "$PSScriptRoot" `
-  -expectedEmbeddedMatch '^Lidarr\.develop\.[\d\.]+\.windows-installer\.exe$' `
+  -expectedEmbeddedMatch '^Lidarr\.(master|develop)\.[\d\.]+\.windows-installer\.exe$' `
   -licenseShouldMatch 'GNU GENERAL PUBLIC LICENSE' `
   -expectedDefaultDirectory "${env:ProgramData}\Lidarr"
