574c3e6931b754d7e63cafb93f628fd6ebfe4ddd
JourneyOver <timtag1190@gmail.com>
2019-12-04 12:15:20 +0000

    AU: 1 updated - renamemytvseries2 [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/512e65966db3bd0bbbdf3da1235274de4caf8361

--- automatic/renamemytvseries2/tools/chocolateyInstall.ps1
+++ automatic/renamemytvseries2/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'renamemytvseries2'
-$url = 'https://www.tweaking4all.com/?wpfb_dl=148'
-$checksum = '043680b06f81f24a1abf374dfbad519189e0fb7da6958d672e1ed09695d56200'
+$url = 'https://www.tweaking4all.com/downloads/video/RenameMyTVSeries-2.0.10-Windows-32bit-setup.exe'
+$checksum = '086b1bd8bce703ae09190948cb48a3e1705e981e3043fb56eed52d411cbbbe83'
 $checksumType = 'sha256'
 
 $packageArgs = @{
