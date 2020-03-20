31ab7619843030ec6e65005e1f751dc765679019
JourneyOver <timtag1190@gmail.com>
2020-03-20 18:08:39 +0000

    AU: 3 updated - CentBrowser sonarr vortex [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/c102469ca0222336afedb8d8acff5702697f8b32

--- automatic/centbrowser/tools/chocolateyInstall.ps1
+++ automatic/centbrowser/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $pp = Get-PackageParameters
 
 $packageName = 'CentBrowser'
-$url = 'http://static.centbrowser.com/beta_32/centbrowser_4.2.7.116.exe'
-$url64 = 'http://static.centbrowser.com/beta_64/centbrowser_4.2.7.116_x64.exe'
-$checksum = '06d9b0127d6e7e61c1d6292b260089108bac2e887c641368d8a90cf5c4a3099a'
-$checksum64 = '8a13a9b19d9860872ce429e392bf2bed298a5104893ff5dce0f622688e0bafec'
+$url = 'http://static.centbrowser.com/installer_32/centbrowser_4.2.7.128.exe'
+$url64 = 'http://static.centbrowser.com/installer_64/centbrowser_4.2.7.128_x64.exe'
+$checksum = '7bc4871447f112e05a7869e629e9c852e1f09389cdf309356d3963b3bf7f21f8'
+$checksum64 = '9307a8fb9591c5017af4947268f40a6d015ba3602f59a0c23fc93bb81362184e'
 $checksumType = 'sha256'
 
 if (!$pp['dir']) { $pp['dir'] = "$env:LOCALAPPDATA" }
