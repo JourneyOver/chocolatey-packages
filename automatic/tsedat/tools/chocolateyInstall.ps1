c9bd7b9d85a288245cc3a7cf8b3d777815392a8d
JourneyOver <timtag1190@gmail.com>
2020-03-31 12:12:06 +0000

    AU: 2 updated - jackett tsedat [skip ci] https://gist.github.com/508bb89c4cc35b67842940c60310532b/abf9a536740e0f5923d25fd8856fdaa5725df5c7

--- automatic/tsedat/tools/chocolateyInstall.ps1
+++ automatic/tsedat/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'tsedat'
-$url = 'https://www.sequencepublishing.com/_files/TheSage_Setup_7-30-2702.exe'
-$checksum = 'cbe03e8d9a776074c28c80c390453a47b7eabcd0fa87d8351922401713a69f79'
+$url = 'https://www.sequencepublishing.com/_files/TheSage_Setup_7-32-2704.exe'
+$checksum = 'e4023f16325cdd8a2f9882cbf2858c09c76426b972b3c82b34e8871e593e0f30'
 $checksumType = 'sha256'
 
 $packageArgs = @{
