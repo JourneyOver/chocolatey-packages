1256f1bcbaa9b06d7139971a2fb094e59a71562d
Journey <timtag1190@gmail.com>
2023-06-23 19:35:10 +0000

    AU: 1 updated - streamlabs-obs [skip ci] https://gist.github.com/JourneyOver/508bb89c4cc35b67842940c60310532b/7cb3402cb9f73c19d340fcf85a0119dc7f73d70f

--- automatic/streamlabs-obs/tools/chocolateyInstall.ps1
+++ automatic/streamlabs-obs/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'streamlabs-obs'
-$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+Desktop+Setup+1.13.1.exe'
-$checksum64 = 'daa4acb3ae7a2f1a724892eb3739d65e42e5b94d3e53ae176c6fd03348003763'
+$url64 = 'https://slobs-cdn.streamlabs.com/Streamlabs+Desktop+Setup+1.13.2.exe'
+$checksum64 = '13e5be2ecde263aecda6686b9a88da18abc28eda00c4b99671ef273bf4bb4f8b'
 $checksumType = 'sha256'
 
 if ((Get-OSArchitectureWidth 32) -or $env:ChocolateyForceX86) {
