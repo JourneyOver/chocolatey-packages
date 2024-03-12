87fe6f4de26446ea63f368322542616891181060
Journey <timtag1190@gmail.com>
2024-03-12 00:59:21 -0500

    @mkevenaar is taking over these packages
    https://github.com/chocolatey-community/chocolatey-package-requests/issues/1498 https://github.com/chocolatey-community/chocolatey-package-requests/issues/1499 https://github.com/chocolatey-community/chocolatey-package-requests/issues/1506 https://github.com/chocolatey-community/chocolatey-package-requests/issues/1507 https://github.com/chocolatey-community/chocolatey-package-requests/issues/1505

--- automatic/jackett/tools/chocolateyBeforeModify.ps1
+++
@@ -1,8 +0,0 @@
-# Stop Jackett service before upgrade/uninstall if running
-$service = "Jackett"
-if (Get-Service "$service" -ErrorAction SilentlyContinue) {
-  $running = Get-Service $service
-  if ($running.Status -eq "Running") {
-    Stop-Service $service
-  }
-}
--- automatic/lidarr/tools/chocolateyBeforeModify.ps1
+++
@@ -1,8 +0,0 @@
-# Stop Lidarr service before upgrade/uninstall if running
-$service = "Lidarr"
-if (Get-Service "$service" -ErrorAction SilentlyContinue) {
-  $running = Get-Service $service
-  if ($running.Status -eq "Running") {
-    Stop-Service $service
-  }
-}
